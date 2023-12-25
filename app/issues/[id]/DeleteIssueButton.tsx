088ccddedbd08a9e5ff0e3d9191ef2d56ad69a63
qian2z <98630339+qian2z@users.noreply.github.com>
2023-12-25 23:50:43 +0800

    Add Confirmation Dialog Box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,7 +1,47 @@
-import { Button } from "@radix-ui/themes";
+import { Cross2Icon, TrashIcon } from "@radix-ui/react-icons";
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogRoot,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button,
+  Flex,
+} from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialogRoot>
+      <AlertDialogTrigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete Issue
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogTitle>Delete Confirmation</AlertDialogTitle>
+        <AlertDialogDescription>
+          Are you confirm to delete this issue? This action cannot be undo.
+        </AlertDialogDescription>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialogCancel>
+            <Button variant="soft" color="gray">
+              <Cross2Icon />
+              Cancel
+            </Button>
+          </AlertDialogCancel>
+          <AlertDialogAction>
+            <Button variant="solid" color="red">
+              <TrashIcon />
+              Delete Issue
+            </Button>
+          </AlertDialogAction>
+        </Flex>
+      </AlertDialogContent>
+    </AlertDialogRoot>
+  );
 };
 
 export default DeleteIssueButton;
